2a9923a68cb251bb93eb2c8febc6054f9bb9f9fe
Marinel Cirstea <marinel.cirstea@gmail.com>
2022-01-09 13:43:18 +0000

    initial setup

---
+++ wp-content/plugins/request-a-brochure/request-a-brochure.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Plugin Name: Request a Brochure
+ * Plugin Description: Request a Brochure
+ * Author: Marinel Cirstea
+ * Author URI: https://www.callmejohn.co.uk
+ * Version: 1.0
+ * Text Domain: request-a-brochure
+ * Domain Path: /languages/
+ */
+?>
+<?php
+
+/**
+ * [ACTIVATION]
+ * create the brochure list table and the brochure request table
+ * if they don't exist
+ */
+function rab_activate()
+{
+}
+
+
+/**
+ * [UNINSTALL]
+ * delete the brochure list table and the brochure request table
+ */
+function rab_uninstall()
+{
+}
+
+
+/**
+ * [ADMIN MENU SETUP]
+ * add the admin menu page
+ */
+function rab_admin_menu_setup()
+{
+}
+
+
+/**
+ * [SHORTCODE]
+ */
+function rab_form_shortcode()
+{
+}
+
+
+/**
+ * [ASSETS REGISTRATION]
+*/
+function rab_assets_registration()
+{
+}
+
+?>
